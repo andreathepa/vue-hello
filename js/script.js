44993c476c76d1c8c1e6f6199f757f39bca3c17c
andreathepa <andreadepa32@gmail.com>
2023-06-07 18:35:11 +0200

    style

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ createApp({
         
         text: '<h1>Ciao Vue benvenuto</h1>',
 
+        text_2: '<h1>Ciao Vue addio</h1>',
+
+
         image: 'https://picsum.photos/200/300',
 
-        immagine
+        immagine: '<img src="https://picsum.photos/200/300" class="bord">'
 
 
       }
+    },
+    methods:{
+        start(){
+
+        }
     }
   }).mount('#app');
