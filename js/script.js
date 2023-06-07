dc98675b8e0267dc38f32c9a713699c373e5575b
andreathepa <andreadepa32@gmail.com>
2023-06-07 23:53:30 +0200

    style button

--- js/script.js
+++ js/script.js
@@ -8,17 +8,22 @@ createApp({
 
         text_2: '<h1>Ciao Vue addio</h1>',
 
-
         image: 'https://picsum.photos/200/300',
 
-        immagine: '<img src="https://picsum.photos/200/300" class="bord">'
+        immagine: ''
 
 
       }
     },
     methods:{
         start(){
+            let number = Math.floor(Math.random() * 200);
+
+            let random_img = `<img src="https://picsum.photos/id/${number}/390/300" alt="image">`;
+            
+            this.immagine= random_img
 
         }
+        
     }
   }).mount('#app');
